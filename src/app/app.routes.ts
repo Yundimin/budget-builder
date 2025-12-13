329dae4cd01ad0ba32b3730f7fb84d08d8567ed0
yunjimin <ge72946@gmail.com>
2025-12-13 13:28:32 +0900

    initial setting , tailwind css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./budget/budget').then(m => m.Budget)
+  },
+];
---
+++ src/app/budget/budget.ts
@@ -0,0 +1,21 @@
+import {
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-budget',
+  imports: [
+  ],
+  templateUrl: './budget.html',
+  styleUrl: './budget.css',
+  standalone: true,
+})
+
+
+export class Budget  {
+
+
+
+
+
+}
